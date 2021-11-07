e2c993ea1eb2740829cb21a0649f73f495d60849
Allan Cezar Almeida Chaves <allancac@gmail.com>
2021-11-07 01:25:38 -0300

    Added the Movie Component to the project

---
+++ src/components/Movie.js
@@ -0,0 +1,17 @@
+const Movie = () => {
+
+  return (
+    <div className='movie-item'>
+      <div>
+        <img src='http://placehold.it/150x150' alt='' />
+      </div>
+      <div className='movie-excerpt'>
+        <h3>Batman</h3>
+        <a class='btn btn-primary' href='#'>Ver detalhes</a>
+      </div>
+    </div>
+
+  )
+}
+
+export default Movie
